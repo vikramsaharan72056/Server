b18b5e9788abfde8dd45db4ccaa3f91aa90ff50b
VIKRAM SINGH <66213553+vikramsaharan72056@users.noreply.github.com>
2023-03-28 21:50:09 +0530

    cors change

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app=express()
 const dotenv=require("dotenv")
 const cors=require("cors")
 app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:3000"
-}))
+app.use(cors())
 const DB = "mongodb+srv://root:root@cluster0.brmgqnv.mongodb.net/Instaclone?retryWrites=true&w=majority"
 dotenv.config()
 mongoose.connect(DB).then(() =>{
