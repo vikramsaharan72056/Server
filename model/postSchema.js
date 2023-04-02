b2d0a68ca5097e97ef390fd2edbee0950a233ab5
vikramsaharan72056 <vikramsaharan72056@gmail.com>
2023-04-02 13:30:36 +0530

    register page added

--- model/postSchema.js
+++ model/postSchema.js
@@ -17,5 +17,54 @@ const postschema=new mongoose.Schema({
         required:true
     }
 })
-const postmodel=mongoose.model("post",postschema)
-module.exports=postmodel
+const Postmodel=mongoose.model("post",postschema)
+
+
+
+
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('not valid email')
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    cpassword:{
+        type:String,
+        required:true,
+        minlength:6
+    }
+});
+
+
+UserSchema.pre("save",async function (next) {
+    this.password = await bcrypt.hash(this.password,12);
+    this.cpassword = await bcrypt.hash(this.cpassword,12);
+    next();
+})
+
+const User = mongoose.model("users",UserSchema);
+
+
+
+module.exports=Postmodel;
+module.exports = User
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,25 +1,53 @@
-const express=require("express")
-const router=express.Router()
-const Postmodel=require("../model/postSchema");
-router.post("/addpost",async(req,resp)=>{
+const express = require("express")
+const router = express.Router()
+const Postmodel = require("../model/postSchema");
+const User = require("../model/postSchema");
+router.post("/addpost", async (req, resp) => {
     console.log(req.body)
-    try{const data=new Postmodel(
-        {
-            name:req.body.name,
-            location:req.body.location,
-            description:req.body.description,
-            image:req.body.image
-        }
-    )
-    result=await data.save()
-    resp.json(result)
+    try {
+        const data = new Postmodel(
+            {
+                name: req.body.name,
+                location: req.body.location,
+                description: req.body.description,
+                image: req.body.image
+            }
+        )
+        const result = await data.save()
+        resp.json(result)
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 })
-router.get("/allpost",async(req,resp)=>{
-    const data=await Postmodel.find()
+router.get("/allpost", async (req, resp) => {
+    const data = await Postmodel.find()
     resp.send(data)
 })
-module.exports=router
+
+
+router.post("/register", async (req, res) => {
+    const { name, email, password, cpassword } = req.body;
+    if (!name || !email || !password || !cpassword) {
+        res.status(422).json({ message: "fill all the details" })
+    }
+    try {
+        const preuser = await User.findOne({ email: email })
+        if (preuser) {
+            res.status(422).json({ message: "this email already exist" })
+        } else if (password !== cpassword) {
+            res.status(422).json({ message: "password and cpassword does not match" })
+        } else {
+            const finalUser = new User({
+                name, email, password, cpassword
+            });
+            const storeData = await finalUser.save()
+            res.status(201).json({ status: 201, storeData })
+
+
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+})
+module.exports = router
