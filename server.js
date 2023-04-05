d2db025d9ebc3bad525fdf7fb0c2119eb5609b47
VIKRAM SINGH <66213553+vikramsaharan72056@users.noreply.github.com>
2023-04-05 08:29:33 +0530

    main file added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const app=express()
+const router = require("./routes/postRoute");
+//const dotenv=require("dotenv")
+const cors=require("cors")
+app.use(express.json())
+app.use(cors())
+const DB = "mongodb+srv://root:root@cluster0.brmgqnv.mongodb.net/Instaclone?retryWrites=true&w=majority"
+//dotenv.config()
+mongoose.connect(DB).then(() =>{
+    console.log("connected to DB")
+}).catch(err =>{
+    console.log(err.message)
+})
+
+
+app.use("/",router);
+app.listen(8080,()=>{
+console.log("app runnning on port 8080")
+})
